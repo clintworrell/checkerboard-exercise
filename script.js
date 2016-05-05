fc283ccdcf830491b55e45afdd5397e1f45753d3
Clint Worrell <github@clintworrell.com>
2016-05-04 21:39:08 -0700

    Complete checkerboard with random colors

--- script.js
+++ script.js
@@ -2,12 +2,8 @@ var body = document.getElementsByTagName("body")[0];
 
 for (var i = 0; i < 81; i++) {
   var tile = document.createElement("div");
-  if (i % 2 === 0) {
-    tile.style.backgroundColor = "black";
-  }
-  else {
-      tile.style.backgroundColor = "red";
-  }
+  var randomHexColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+  tile.style.backgroundColor = randomHexColor;
   tile.style.width = "11.1%";
   tile.style.paddingBottom = "11.1%";
   tile.style.float = "left";
