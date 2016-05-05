653511ce58408a09700d4f94646d74847ae543de
Clint Worrell <github@clintworrell.com>
2016-05-04 18:44:03 -0700

    Complete basic checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your JS goes here
+var body = document.getElementsByTagName("body")[0];
+
+for (var i = 0; i < 81; i++) {
+  var tile = document.createElement("div");
+  if (i % 2 === 0) {
+    tile.style.backgroundColor = "black";
+  }
+  else {
+      tile.style.backgroundColor = "red";
+  }
+  tile.style.width = "11.1%";
+  tile.style.paddingBottom = "11.1%";
+  tile.style.float = "left";
+  body.appendChild(tile);
+}
