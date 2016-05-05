b30e36ab81cf159125f9d5b75fe6ffe5615caf74
Clint Worrell <github@clintworrell.com>
2016-05-05 13:14:27 -0700

    Complete flashing checkerboard

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 var body = document.getElementsByTagName("body")[0];
 
-var cyanAlpha = 1;
-var blueAlpha = 1;
-for (var i = 0; i < 81; i++) {
-  var tile = document.createElement("div");
-  if (i % 2 === 0) {
-    tile.style.backgroundColor = "rgba(62, 211, 253, " + blueAlpha + ")";
-    blueAlpha -= 0.015;
+function randomColorCheckerboard () {
+  for (var i = 0; i < 81; i++) {
+    var tile = document.createElement("div");
+    var randomHexColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    tile.style.backgroundColor = randomHexColor;
+    tile.style.width = "11.1%";
+    tile.style.paddingBottom = "11.1%";
+    tile.style.float = "left";
+    body.appendChild(tile);
   }
-  else {
-    tile.style.backgroundColor = "rgba(207, 44, 248, " + cyanAlpha + ")";
-    cyanAlpha -= 0.015;
-  }
-  tile.style.width = "11.1%";
-  tile.style.paddingBottom = "11.1%";
-  tile.style.float = "left";
-  body.appendChild(tile);
 }
+
+function flashing () {
+  var tiles = document.getElementsByTagName("div");
+  for (var i = 0; i < tiles.length; i++) {
+    var tile = document.createElement("div");
+    var randomHexColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    tiles[i].style.backgroundColor = randomHexColor;
+    body.appendChild(tiles[i]);
+  }
+}
+
+randomColorCheckerboard();
+setInterval(flashing, 2000);
