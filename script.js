dce11c59788a27e3658b5da53e1fd65a9c1c22a8
Clint Worrell <github@clintworrell.com>
2016-05-04 22:24:42 -0700

    Gradient checkerboard complete

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var body = document.getElementsByTagName("body")[0];
 
+var cyanAlpha = 1;
+var blueAlpha = 1;
 for (var i = 0; i < 81; i++) {
   var tile = document.createElement("div");
-  var randomHexColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-  tile.style.backgroundColor = randomHexColor;
+  if (i % 2 === 0) {
+    tile.style.backgroundColor = "rgba(62, 211, 253, " + blueAlpha + ")";
+    blueAlpha -= 0.015;
+  }
+  else {
+    tile.style.backgroundColor = "rgba(207, 44, 248, " + cyanAlpha + ")";
+    cyanAlpha -= 0.015;
+  }
   tile.style.width = "11.1%";
   tile.style.paddingBottom = "11.1%";
   tile.style.float = "left";
